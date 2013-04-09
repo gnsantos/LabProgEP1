8d3878b2f7897a3fb00bd1bdb6e3588176e3f6a8
Gervasio Santos <gervasio.neto94@gmail.com>
2013-04-09 18:48:23 -0300

    func em c para call ass

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int testaarmstrong (int);
+
+int main (int argc, char *argv[]) {
+  int n;
+  printf("Entre um numero: ");
+  scanf("%d", &n);
+  if (testaarmstrong(n)) {
+    puts("Eh Armstrong");
+  }
+  else
+    puts("Nao eh armstrong");
+
+  return 0;
+
+}
