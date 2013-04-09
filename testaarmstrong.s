62dc4bae0e303840f7a496370b6afb0abb52c549
Gervasio Santos <gervasio.neto94@gmail.com>
2013-04-09 18:47:52 -0300

    main de assembly

---
+++ testaarmstrong.s
@@ -0,0 +1,129 @@
+.intel_syntax noprefix
+.global testaarmstrong
+
+a = 8
+b = 12
+n = 8
+f = 8
+numdigit = -16
+digit = -8
+sum = -12
+aux = -4
+
+
+	
+descobredigitos:
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp+n]	# x = n		
+	mov ecx, 0		# cont = 0
+	mov ebx, 10
+	mov edx, 0
+	
+loop:	
+	cmp eax, 0		# while( x != 0 )	
+	je termino		# jump to termino if x == 0	
+	inc ecx			# cont++
+	div ebx			# x / = 10
+	mov edx, 0
+	jmp loop
+	
+termino:
+	mov eax, ecx
+	pop ebp
+	ret
+
+	
+
+
+
+	
+	
+potencia:
+	push ebp		
+	mov ebp, esp		
+	mov ecx, [ebp+b]	
+	dec ecx			
+	mov ebx, [ebp+a]	
+	mov eax, ebx	
+	
+looop:
+	mul ebx			
+	dec ecx		
+	cmp ecx, 0		
+	ja looop			
+
+fim:
+	pop ebp			
+	ret			
+
+
+
+
+testaarmstrong:
+	push ebp
+	#push eax
+	mov ebp, esp
+
+	sub esp, 16
+
+	mov eax, [ebp+f]
+	mov [ebp+aux], eax
+	mov edx, 0
+	mov ecx, 0
+	mov ebx, 0
+
+	mov ebx, [ebp+f]
+	push ebx
+	#push eax
+	call descobredigitos
+
+	mov [ebp+numdigit], eax
+	mov edx, 0
+	mov [ebp+sum], edx
+
+laco:
+	mov edx, [ebp+aux]
+	cmp edx, 0
+	je end
+
+	mov edx, 0
+	mov ecx, 0
+	mov ebx, 10
+	mov eax, [ebp+aux]
+
+	div ebx
+
+	mov [ebp+aux], eax
+	mov [ebp+digit], edx
+
+	push [ebp+numdigit]
+	push [ebp+digit]
+
+	call potencia
+
+	mov ecx, eax
+	mov eax, [ebp+sum]
+	add eax, ecx
+	mov [ebp+sum], eax
+
+	jmp laco
+	
+
+end:
+	add esp, 16
+	#mov eax, [ebp+sum]
+	cmp eax, [ebp+f]
+	je returntrue
+	#pop eax
+	mov eax, 0
+	pop ebp
+	ret
+
+returntrue:
+	#pop eax
+	mov eax, 1
+	#pop eax
+	pop ebp
+	ret
+	
