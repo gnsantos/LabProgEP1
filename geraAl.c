c76ea598a3ef10a54c40a5193999634ad486b391
Gervasio Santos <gervasio.neto94@gmail.com>
2013-04-10 19:59:55 -0300

    adequado

--- geraAl.c
+++ geraAl.c
@@ -1,36 +1,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-FILE* randomNumberList(int seed, int n, char name[]){
-  unsigned int random;
-  int i;
-  FILE *saida;
-  srand(seed);
-  saida = fopen(name, "w");
-  for(i = 0; i < n; i++){
-    random = (unsigned int) rand();
-    fprintf(saida, "%d ", random);
-  }
-  return saida;
-}
+#include <math.h> 
 
 int main(int argc, char* argv[]){
 	FILE *saida;
 	int i = 0;
 	
-	char* name = argv[1];
-	int n = atoi(argv[2]);
-	int s = atoi(argv[3]);
+	char modo = argv[1][0];
+	char* name = argv[2];
+	int n = atoi(argv[3]);
+	int s = atoi(argv[4]);
 	
-	srand(s);
-  	saida = fopen(name, "w");
-  	for(i = 0; i < n; i++){
-  		int random = rand();
-  		fprintf(saida, "%d ", random);
- 	 }
-
+	if (modo == 's') {
+	  saida = fopen(name, "w");
+	  for (i = 0; i<n; i++) {
+	    fprintf(saida,"%d ", i);
+	  }
+	}
+	else {
+	  srand(s);
+	  saida = fopen(name, "w");
+	  for(i = 0; i < n; i++){
+	    int random = rand();
+	    fprintf(saida, "%d ", random);
+	  }
+	}
 	fclose(saida);
 	return 0;
 }
